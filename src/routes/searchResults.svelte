c3c4455e152cebafb4aaa012c98e61b66827b2e6
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-04-19 15:10:44 +1000

    1

--- src/routes/searchResults.svelte
+++ src/routes/searchResults.svelte
@@ -1,26 +1,26 @@
-<script>
-	import { onMount } from 'svelte';
+<script context="module">
 	import axios from 'axios';
-	import { auth } from '@src/utils/store';
-	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
-
-	var error = null;
-	var searchResults = null;
-
-	onMount(async function () {
-		var searchTerm = $page.url.searchParams.get('searchTerm').trim()
+	export async function load({ url }) {
+		var props;
+		var searchTerm = url.searchParams.get('searchTerm').trim();
 		var res = await axios.post('/api/product/getManyByTitle', { searchTerm });
 		if (res.data.error) {
-			error = res.data.error;
+			props = { ...props, error: res.data.error };
 		}
-		searchResults = res.data.products;
-	});
+		props = { ...props, searchResults: res.data.products };
+		return {
+			props
+		};
+	}
 </script>
 
-{#if searchResults}
-	<div class="bg-white text-black">
-		{JSON.stringify(searchResults)}
-	</div>
-{/if} 
+<script>
+	export var searchResults;
+	export var error;
+</script>
 
+<svelte:head><title>Search Results</title></svelte:head>
+
+<div class="bg-white text-black">
+	{JSON.stringify(searchResults)}
+</div>
