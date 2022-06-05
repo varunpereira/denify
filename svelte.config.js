08e75c3d99fa6c9f6de678c6b9c1b68ed7377dbc
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-06-05 12:17:24 +1000

    1

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
 import path from 'path';
 import adapter from '@sveltejs/adapter-auto';
-// import {websocketBackend}  from './src/utils/websocketBackend';
-import { Server } from 'socket.io';
 
 const config = {
 	kit: {
@@ -11,38 +9,7 @@ const config = {
 				alias: {
 					'@src': path.resolve('./src')
 				}
-			},
-			plugins: [
-				// only works locally not on vercel
-				// websocketBackend,
-				{
-					name: 'sveltekit-socket-io',
-					configureServer(server) {
-						var io = new Server(server.httpServer);
-						io.on('connection', function (websocket) {
-							// 1 way data com
-							var username = `User ${Math.round(Math.random() * 999999)}`;
-							websocket.emit('name', username);
-							// 2 way data com
-							websocket.on('message', function (message) {
-								io.emit('message', {
-									from: username,
-									message: message,
-									time: new Date().toLocaleString()
-								});
-							});
-							// // chat api
-							// websocket.on('newMessage', function (newMessage) {
-							// db query here
-							// 	var messages = []
-							// 	io.emit('messages', messages);
-							// });
-						});
-						// done
-						console.log('SocketIO injected');
-					}
-				}
-			]
+			}
 		}
 	}
 };
