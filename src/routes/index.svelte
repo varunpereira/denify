b69ded1b8272948a60cea57e54b986f347ad300e
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-08-10 12:52:41 +1000

    1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,13 +24,15 @@
 			error = res.data.error;
 		}
 		products = res.data.products;
-		console.table("products")
+		console.table('products');
 	}
 </script>
 
 <svelte:head><title>Home - Denify</title></svelte:head>
 
 <img src="/homeBanner.png" alt="" class="w-full rounded-md mb-4" />
+
+<p class="mb-5 text-xl font-semibold flex justify-center">Trending</p>
 
 {#if products}
 	<div class="flex flex-wrap text-white">
