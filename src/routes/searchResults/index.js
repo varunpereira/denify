4de8bfc152e1f4cea0f541b3af72ea8e138c6e2d
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-08-22 16:51:11 +1000

    1

--- src/routes/searchResults/index.js
+++ src/routes/searchResults/index.js
@@ -4,9 +4,10 @@ import productModel from '@src/models/product';
 db();
 
 export async function post({ request }) {
-	var {searchTerm} = await request.json();
+	var { searchTerm } = await request.json();
 	var products = await productModel.find({
-		title: { $regex: searchTerm, $options: 'i' }
+		title: { $regex: searchTerm, $options: 'i' },
+		approved: 'true'
 	});
 	return {
 		body: {
