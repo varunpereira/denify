872cc94c922206ddbc0280d5ccaadc14ce2e6e28
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-08-23 13:13:12 +1000

    1

--- src/routes/product/sell/status.svelte
+++ src/routes/product/sell/status.svelte
@@ -8,22 +8,33 @@
 
 	var error = null;
 	var productId = $page.url.searchParams.get('productId').trim();
+	var product = null;
 
 	onMount(async function () {
 		if (cookie.get('auth')) {
 			$auth = JSON.parse(cookie.get('auth'));
 		}
 		productId = $page.url.searchParams.get('productId').trim();
-		
+		var res = await axios.post($page.url.pathname, {
+			productId
+		});
+		if (res.data.error) {
+			error = res.data.error;
+		}
+		product = res.data.product;
 	});
 </script>
 
 <svelte:head><title>Sell Status - Denify</title></svelte:head>
 
-<div
-	class="container mx-auto flex max-w-md flex-1 flex-col  px-2 w-full rounded bg-white  px-20 py-8 text-black"
->
-	<p class="mb-8 text-center text-2xl">Sell Status for Product {productId}</p>
-	<p>Pending.</p>
-	<p>You can view status of all your sales in your account's sales page.</p>
-</div>
+{#if product}
+	<div
+		class="container mx-auto flex max-w-md flex-1 flex-col  px-2 w-full rounded bg-white  px-20 py-8 text-black"
+	>
+		<p class="mb-8 text-center text-2xl">Sell Status for Product {productId}</p>
+		<p>Pending.</p>
+		<p>You can view status of all your sales in your account's sales page.</p>
+	</div>
+{:else}
+	<p>Product does not exist.</p>
+{/if}
