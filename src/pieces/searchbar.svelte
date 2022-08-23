6e1e844442db57e3a27b3ebbb010992dcb2637ef
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-08-23 10:12:27 +1000

    1

--- src/pieces/searchbar.svelte
+++ src/pieces/searchbar.svelte
@@ -4,7 +4,8 @@
 
 	var searchTerm = '';
 	var dropdownSelected = false;
-	var filterList = ['All', 'Clothes','Tech'];
+	var filterList = ['All', 'Clothes', 'Tech'];
+	var selected = null;
 </script>
 
 <div class="mr-5 max-h-min w-full min-w-min pb-1 md:pb-0 shadow-md md:flex text-black">
@@ -14,22 +15,24 @@
 			goto('/searchResults?searchTerm=' + searchTerm);
 		}}
 	>
-		<button
-			on:click|preventDefault={function () {
-				dropdownSelected = !dropdownSelected;
-			}}
-			type="button"
+		<select
+			bind:value={selected}
 			class="absolute inset-y-0 w-10 bg-gray-100 py-2 px-4 rounded-l-md"
 		>
-			{filterList[0]}
-		</button>
+			{#each filterList as filter}
+				<option value={filter} >
+					{filter}
+				</option>
+			{/each}
+			
+		</select>
 		<input
 			value={searchTerm}
 			on:input={function (event) {
 				searchTerm = event.target.value;
 			}}
 			type="text"
-			placeholder="search"
+			placeholder={selected}
 			class="focus:shadow-outline w-full min-w-max bg-white py-2 pl-12 text-sm leading-tight text-black focus:outline-none rounded-md"
 		/>
 		{#if searchTerm.trim() != ''}
@@ -51,15 +54,4 @@
 			<SearchIcon class="h-4 w-4 text-black" />
 		</button>
 	</form>
-	{#if dropdownSelected}
-	<div class="inset-y-0 "></div>
-		{#each filterList as filter}
-			<button
-				type="button"
-				class="bg-white block py-2 rounded hover:bg-gray-300"
-			>
-				{filter}
-			</button>
-		{/each}
-	{/if}
 </div>
