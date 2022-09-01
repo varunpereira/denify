fa1ed9a0dc47bdf13a3cc2ddd84d0e3420b9450e
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-09-01 13:34:26 +1000

    1

--- src/pieces/navbar.svelte
+++ src/pieces/navbar.svelte
@@ -98,9 +98,9 @@
 							class="h-8 rounded-full pt-2 md:pt-0 md:flex md:flex-wrap md:justify-center"
 						>
 							<UserIcon class="h-5 w-5 border border-1 rounded-full" />
-							<div class="flex justify-start">
+							<div class="grid gap-x-2">
 								{$auth.user.email}
-								<ChevronDownIcon class="w-2 h-2"/>
+								<ChevronDownIcon class="w-4 h-4"/>
 							</div>
 						</button>
 						{#if dropdownSelected}
