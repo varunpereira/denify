3cffe73a37bf6516a5c2c6772f26d30e5b79cd05
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-09-03 16:19:54 +1000

    1

--- src/pieces/searchbar.svelte
+++ src/pieces/searchbar.svelte
@@ -11,14 +11,14 @@
 	var pagination = '1';
 	var suggestionsOff = false;
 
-	$: if (
-		$page.url.searchParams.get('searchTerm') &&
-		searchTerm != $page.url.searchParams.get('searchTerm')
-	) {
-		searchTerm = $page.url.searchParams.get('searchTerm').trim();
-	}
-
 	$: {
+		suggestionsOff = true;
+		if (
+			$page.url.searchParams.get('searchTerm') &&
+			searchTerm != $page.url.searchParams.get('searchTerm')
+		) {
+			searchTerm = $page.url.searchParams.get('searchTerm').trim();
+		}
 		suggestionsOff = false;
 		suggestions = 'loading';
 		axios
