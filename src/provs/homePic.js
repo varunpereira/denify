f7266b5d279ba16eff396480e6d6792c3c12d22b
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-09-14 14:19:10 +1000

    1

--- src/provs/homePic.js
+++ src/provs/homePic.js
@@ -1,11 +1,11 @@
 export var pic = [
 	'/home/0.png',
-	'/home/1.jpg',
+	// '/home/1.jpg',
 	'/home/2.jpg',
 	'/home/3.jpg',
-	'/home/4.jpg',
+	// '/home/4.jpg',
 	'/home/5.jpg',
 	'/home/6.jpg',
 	'/home/7.jpg',
-	'/home/8.jpg'
+	// '/home/8.jpg'
 ];
